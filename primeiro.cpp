451ac0983cb2d84a4031985073e5dd8c6ee2a962
Andriele Gaudencio <33098123+andrielegaudencio@users.noreply.github.com>
2023-05-09 15:29:12 -0300

    Add files via upload
    Meu primeiro código em C++

---
+++ primeiro.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+using namespace std;
+main(){
+	char nome[80];
+	int idade;
+	cout << "Nome? ";
+	cin>>nome;
+	
+	cout<<"Idade? ";
+	cin>>idade;
+	
+	cout<<"\nOla, "<<nome<<", tudo OK aos";
+	cout<<idade<<"anos?\n"<<endl;
+}
